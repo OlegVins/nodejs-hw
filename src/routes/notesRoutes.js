4a11b03ac3256ee4cac5d1113f58d24503d112aa
OlegVins <olegvinnickii@gmail.com>
2026-06-03 21:27:52 +0200

    fix files

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -7,16 +7,18 @@ import {
   deleteNote,
   updateNote,
 } from '../controllers/notesController.js';
-import { getNoteSchema } from '../validations/notesValidation.js';
-import { createNotesSchema } from '../validations/notesValidation.js';
-import { noteIdSchema } from '../validations/notesValidation.js';
-import { updateNoteSchema } from '../validations/notesValidation.js';
+import {
+  getAllNotesSchema,
+  createNoteSchema,
+  noteIdSchema,
+  updateNoteSchema,
+} from '../validations/notesValidation.js';
 
 const router = Router();
 
-router.get('/notes', celebrate(getNoteSchema), getAllNotes);
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
-router.post('/notes', celebrate(createNotesSchema), createNote);
+router.post('/notes', celebrate(createNoteSchema), createNote);
 router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
 router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,15 +1,16 @@
 import { Joi, Segments } from "celebrate";
 import { isValidObjectId } from "mongoose";
+import { TAGS } from '../constants/tags.js';
 
-export const getNoteSchema = {
+export const getAllNotesSchema = {
   [Segments.QUERY]:
     Joi.object({
       page:
         Joi.number().integer().min(1).default(1),
       perPage:
-        Joi.number().integer().min(5).max(15).default(10),
+        Joi.number().integer().min(5).max(20).default(10),
       tag:
-        Joi.string().valid('Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'),
+        Joi.string().valid(...TAGS),
       search:
       Joi.string().trim().allow('')
     }),
@@ -30,9 +31,9 @@ export const updateNoteSchema = {
       title:
         Joi.string().min(1),
       content:
-        Joi.string().valid(''),
+        Joi.string().allow(''),
       tag:
-        Joi.string().valid('Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'),
+        Joi.string().valid(...TAGS),
     }).min(1),
 };
 
@@ -44,14 +45,14 @@ export const noteIdSchema = {
     }),
 };
 
-export const getAllNotesSchema = {
+export const createNoteSchema = {
   [Segments.BODY]:
     Joi.object({
       title:
         Joi.string().min(1).required(),
       content:
-        Joi.string().valid(''),
+        Joi.string().allow(''),
       tag:
-        Joi.string().valid('Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo')
+        Joi.string().valid(...TAGS),
     }),
 };
